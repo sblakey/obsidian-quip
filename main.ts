e7fb9e43b4cbf924c1b999b34c3cee216ee648a4
Sean Blakey <pythonista@gmail.com>
2022-12-30 18:41:30 -0800

    Move SettingsTab to settings.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,7 @@
 import { App, CachedMetadata, FileSystemAdapter, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
 import { QuipAPIClientError, QuipAPIClient } from './quipapi';
 import render from './renderer';
+import QuipSettingTab from './settings';
 
 // Remember to rename these classes and interfaces!
 
@@ -196,74 +197,6 @@ export class SuccessModal extends Modal {
 	}
   }
 
-class QuipSettingTab extends PluginSettingTab {
-	plugin: QuipPlugin;
-
-	constructor(app: App, plugin: QuipPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for publishing to Quip.'});
-
-		new Setting(containerEl)
-			.setName('Personal API Token')
-			.setDesc('Obtained from /dev/token on your Quip website')
-			.addText(text => text
-				.setValue(this.plugin.settings.token)
-				.onChange(async (value) => {
-					console.log(`Token: ${value}`);
-					this.plugin.settings.token = value;
-					await this.plugin.saveSettings();
-				}));
-		new Setting(containerEl)
-			.setName('API hostname')
-			.setDesc('Endpoint for calls to the Quip automation API')
-			.addText(text => text
-				.setPlaceholder('platform.quip.com')
-				.setValue(this.plugin.settings.hostname)
-				.onChange(async (value) => {
-					console.log(`Hostname: ${value}`);
-					this.plugin.settings.hostname = value;
-					await this.plugin.saveSettings();
-				}));
-		new Setting(containerEl)
-			.setName('Remove YAML front matter')
-			.setDesc('Strip leading YAML out of notes before sending to Quip')
-			.addToggle(toggle => toggle
-				.setValue(this.plugin.settings.removeYAML)
-				.onChange(async (value) => {
-					console.log(`Remove YAML: ${value}`);
-					this.plugin.settings.removeYAML = value;
-					await this.plugin.saveSettings();
-				}));
-		new Setting(containerEl)
-			.setName('Add Quip link')
-			.setDesc('Insert a link to the published Quip document into YAML front matter')
-			.addToggle(toggle => toggle
-				.setValue(this.plugin.settings.addLink)
-				.onChange(async (value) => {
-					console.log(`Add Link: ${value}`);
-					this.plugin.settings.addLink = value;
-					await this.plugin.saveSettings();
-				}));
-		new Setting(containerEl)
-			.setName('Inline embedded notes')
-			.setDesc('Replace embed-links with the content of those notes')
-			.addToggle(toggle => toggle
-				.setValue(this.plugin.settings.inlineEmbeds)
-				.onChange(async (value) => {
-					console.log(`Add Link: ${value}`);
-					this.plugin.settings.inlineEmbeds = value;
-					await this.plugin.saveSettings();
-				}));
-	}
-}
 
 
 async function preProcessMarkdown(plugin: QuipPlugin, file: TFile): Promise<string> {
