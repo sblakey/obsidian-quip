b32aeec37bcc1e17ba80bd7724454f503753c52d
Sean Blakey <sblakey@amazon.com>
2023-03-03 18:03:29 -0800

    Moved SuccessModal to it;s own file

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,11 @@
-import { App, htmlToMarkdown, MarkdownView, Modal, normalizePath, Notice, Plugin, sanitizeHTMLToDom, Setting, stringifyYaml, TFile, Vault } from 'obsidian';
+import { htmlToMarkdown, MarkdownView, normalizePath, Notice, Plugin, sanitizeHTMLToDom, Setting, stringifyYaml, TFile, Vault } from 'obsidian';
 import { QuipAPIClient } from './quipapi';
 import render from './renderer';
 import { DEFAULT_SETTINGS, QuipPluginSettings, QuipSettingTab } from './settings';
 import TurndownService from 'turndown';
 import { gfm } from 'turndown-plugin-gfm';
 import { ImportModal } from './ImportModal';
+import { SuccessModal } from './SuccessModal';
 
 
 interface QuipFrontMatter {
@@ -210,28 +211,3 @@ ${markdown}`;
 }
 
 
-export class SuccessModal extends Modal {
-	link: string;
-
-	constructor(app: App, link: string) {
-		super(app);
-		this.link = link;
-	}
-
-	onOpen() {
-		const { contentEl } = this;
-		//contentEl.setText(`Successfully published to ${this.link}`);
-		contentEl.createEl('span', null, (span) => {
-			span.innerText = 'Successfully published to ';
-			span.createEl('a', null, (anchor) => {
-				anchor.href = this.link;
-				anchor.innerText = this.link;
-			});
-		})
-	}
-
-	onClose() {
-		const { contentEl } = this;
-		contentEl.empty();
-	}
-}
